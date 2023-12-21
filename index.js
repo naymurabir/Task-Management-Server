b6eb5d8efa25f6ec9c79b5755116048c3786881a
naymurabir <naymurrahman.abir@gmail.com>
2023-12-21 19:44:38 +0600

    Task management server and database is created

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+var cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+//Middleware 
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+//Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cgjyfgp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const usersCollection = client.db("visionTaskDB").collection("users")
+
+        //JWT related APIs
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+
+            }).send({ success: true })
+        })
+
+        //Remove token after logout the user
+        app.post('/logout', async (req, res) => {
+            const user = req.body
+            console.log("User: ", user);
+            res.clearCookie('token', {
+                maxAge: 0,
+                secure: process.env.NODE_ENV === 'production' ? true : false,
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            })
+                .send({ status: true })
+        })
+
+        //------------------Users related APIs-------------------
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const query = { email: user.email }
+            const existUser = await usersCollection.findOne(query)
+            if (existUser) {
+                return res.send({ message: "User already exists.", insertedId: null })
+            } else {
+                const result = await usersCollection.insertOne(user)
+                res.send(result)
+            }
+        })
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("The Vision Task server is running successfully...")
+})
+
+app.listen(port, () => {
+    console.log(`The server is running on port: ${port}`);
+})
